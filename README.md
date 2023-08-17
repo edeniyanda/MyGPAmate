1b73de8d01d36852529d545558191aac6d5429f2
Eden Iyanda <edeniyanda@gmail.com>
2023-08-17 04:50:36 +0100

    Add Toggle Widgets

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # MyGPAmate
+
+
+<a href="https://www.flaticon.com/free-icons/dashboard" title="dashboard icons">Dashboard icons created by Eucalyp - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/dashboard" title="dashboard icons">Dashboard icons created by Eucalyp - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/settings" title="settings icons">Settings icons created by Pixel perfect - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/course" title="course icons">Course icons created by Freepik - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Freepik - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/grades" title="grades icons">Grades icons created by Freepik - Flaticon</a>
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import PySide6
 import qdarkstyle
 import sys
 import sqlite3
+from functools import partial
 
 
 
@@ -25,14 +26,26 @@ class MainApp(QMainWindow, ui):
         
     def Handle_Ui_Changes(self):
         self.tabWidget_main.setCurrentIndex(0)
+        self.tabWidget_main_app.setCurrentIndex(0)
         self.tabWidget_main.tabBar().setVisible(False)
-        ...
-    def Handle_Button(self):
-        self.pushButton_get_started.clicked.connect(self.change_main_widget_index)
-        ...    
+        self.tabWidget_main_app.tabBar().setVisible(False)
         
-    def change_main_widget_index(self):
-        self.tabWidget_main.setCurrentIndex(1)
+    def Handle_Button(self):
+        self.pushButton_get_started.clicked.connect(partial(self.change_welcome_widget_index, 1))        
+        self.pushButton_dash_board.clicked.connect(partial(self.change_main_app_widget, 0))
+        self.pushButton_courses.clicked.connect(partial(self.change_main_app_widget, 1))
+        self.pushButton_grade.clicked.connect(partial(self.change_main_app_widget, 2))
+        self.pushButton_profile.clicked.connect(partial(self.change_main_app_widget, 3))
+        self.pushButton_settings.clicked.connect(partial(self.change_main_app_widget, 4))
+        ...    
+    
+    # Change Main Widget 
+    def change_welcome_widget_index(self, index_position:int):
+        self.tabWidget_main.setCurrentIndex(index_position)
+        
+    # Change Main Application Widget
+    def change_main_app_widget(self, index_position:int):
+        self.tabWidget_main_app.setCurrentIndex(index_position)
         
 
 def main():
